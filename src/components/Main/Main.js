3c8fd4c352562d8303a656e4172975edc757fa58
Mykyta <bolelod@gmail.com>
2022-01-09 12:47:00 +0200

    add Main component and change Main page to Home page

---
+++ src/components/Main/Main.js
@@ -0,0 +1,20 @@
+import './Main.css';
+
+const Main = () => {
+  return (
+    <main className='main'>
+      <article>
+        <div>Super Quiz</div>
+        <div>Super Quiz</div>
+        <div>Super Quiz</div>
+        <div>Super Quiz</div>
+        <div>Super Quiz</div>
+        <div>Super Quiz</div>
+        <div>Super Quiz</div>
+        <div>Super Quiz</div>
+      </article>
+    </main>
+  );
+};
+
+export default Main;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Main from '../../components/Main/Main';
 
 const Home = () => {
   return (
     <React.Fragment>
       <Header />
+      <Main />
     </React.Fragment>
   );
 };
